06426e5721956fee2085bfab30a285dbeb291aa1
jithendernanaboina <jithendernanaboina@gmail.com>
2025-05-31 14:20:53 +0530

    changes in the generlogoy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import "./App.css"
 import Genealogy from "./components/tree";
-import Welcomepage from "./components/welcomepage";
 import "./index.css"
 function App() {
-  // const [token, setToken]=useState("")
-  // useEffect(()=>{
-  //   const hash=window.location.hash;
-  //   const _token=hash.split("&")[0].split("=")[1]
-  //   window.localStorage.setItem("token",_token);
-  //   setToken(_token)
-
-  // },[])
   return (
    <div>
-{/* <Welcomepage/> */}
 <Genealogy/>
-
    </div>
-   
-    
-   
-  
- 
   )
-     
-   
+
 }
 
 export default App;
